95714d5e9a02947bec9dc05930c285fc41871d7d
nicolasmelob7 <nicolasmelob7@gmail.com>
2026-03-01 21:26:05 -0300

    desenvolvimento menu

--- angular-project-app/src/app/app.component.ts
+++ angular-project-app/src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { SidebarComponent } from '@app/components/sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [SidebarComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-project-app';
+  isSidebarCollapsed = signal(false);
+  faArrowLeft = signal(faArrowLeft).asReadonly();
+  faArrowRight = signal(faArrowRight).asReadonly();
+
+  onSidebarToggle() {
+    this.isSidebarCollapsed.set(!this.isSidebarCollapsed);
+  }
 }
---
+++ angular-project-app/src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,59 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, input, output, signal } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import {
+  faChevronDown,
+  faCoffee,
+  IconDefinition,
+} from '@fortawesome/free-solid-svg-icons';
+
+interface MenuItem {
+  icon: IconDefinition;
+  label: string;
+  children?: MenuItem[];
+  isOpen?: boolean;
+}
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [CommonModule, FaIconComponent],
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+})
+export class SidebarComponent {
+  isSidebarCollapsed = input.required<boolean>();
+  faChevronDown = signal(faChevronDown).asReadonly();
+
+  menuItems = [
+    {
+      icon: faCoffee,
+      label: 'Dashboard',
+      isOpen: false,
+      children: [
+        { icon: faCoffee, label: 'Analytics' },
+        { icon: faCoffee, label: 'Projects' },
+      ],
+    },
+    {
+      icon: faCoffee,
+      label: 'Settings',
+      isOpen: false,
+      children: [
+        { icon: faCoffee, label: 'Profile' },
+        { icon: faCoffee, label: 'Security' },
+      ],
+    },
+    {
+      icon: faCoffee,
+      label: 'Messages',
+    },
+  ];
+
+  toggleMenuItem(item: MenuItem) {
+    // Only toggle if sidebar is not collapsed and item has children
+    if (!this.isSidebarCollapsed() && item.children) {
+      item.isOpen = !item.isOpen;
+    }
+  }
+}
