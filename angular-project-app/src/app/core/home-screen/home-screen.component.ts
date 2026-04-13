8885053c98f71090603f775a9321385d3c82c42a
nicolasmelob7 <nicolasmelob7@gmail.com>
2026-04-13 01:21:28 -0300

    simulate

--- angular-project-app/src/app/core/home-screen/home-screen.component.ts
+++ angular-project-app/src/app/core/home-screen/home-screen.component.ts
@@ -1,20 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { TypewriterComponent } from '@src/app/shared/components/typewriter/typewriter.component';
-
-const phrase1 = 'Hey!';
-const phrase2 = 'Do you wanna play a game?';
-const phrase3 = 'or not?!';
-const phrase4 = "c'mon ...";
-
-const phrases = [phrase1, phrase2, phrase3, phrase4];
+import { Component, OnDestroy, signal } from '@angular/core';
+import { interval, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-home-screen',
   standalone: true,
-  imports: [TypewriterComponent],
   templateUrl: './home-screen.component.html',
   styleUrl: './home-screen.component.scss',
 })
-export class HomeScreenComponent {
-  phrases = signal(phrases).asReadonly();
-}
+export class HomeScreenComponent {}
